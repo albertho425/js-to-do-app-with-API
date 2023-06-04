f09194f633d1e606a0d89d17be619bc7d6a97e42
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-06-04 15:03:58 -0700

    Fix issue of description disappering during edit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+/**
+ * Declare all variables
+ */
+
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
@@ -6,7 +11,12 @@ let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
-let data = [];
+//local storage
+let data = [{}];
+
+/**
+ * Add event listener and validate form
+ */
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -46,42 +56,56 @@ let acceptData = () => {
   data.push({
     text: textInput.value,
     date: dateInput.value,
-    progress: progressInput.value,
     description: textarea.value,
+    progress: progressInput.value,
   });
 
+  console.log("accepting task: " + textInput.value);
   console.log("accepting date: " + dateInput.value);
-  console.log("accepting progress: " + progressInput.value);
   console.log("accepting description: " + textarea.value);
+  console.log("accepting progress: " + progressInput.value);
 
   localStorage.setItem("data", JSON.stringify(data));
 
-  console.log(data);
+  console.log(JSON.stringify(data));
   createTasks();
 };
 
 /**
- * 
+ *  Display template and push data into the template. Reset the form. Also displays the edit/delete buttons
  */
 
 let createTasks = () => {
+  console.log(JSON.stringify(data));
     tasks.innerHTML = "";
     data.map((x, y) => {
       return (tasks.innerHTML += `
       <div id=${y}>
             <span class="fw-bold">${x.text}</span>
             <span class="small text-secondary">${x.date}</span>
-            <span class="small text-secondary">${x.progress}</span>
-            <p>${x.description}</p>
-    
+            
+            <span>${x.description}</span>
+            <span class="">${x.progress}</span>
+
             <span class="options text-center">
-              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="bi bi-pencil-square crud"></i></i>
-              <i onClick ="deleteTask(this);createTasks()" class="bi bi-trash crud"></i>
+              <i onClick= "editTask(this)" 
+                 data-bs-toggle="modal" 
+                 data-bs-target="#form" 
+                 class="bi bi-pencil-square crud">
+              </i>
+              <i onClick ="deleteTask(this);createTasks()" 
+                 class="bi bi-trash crud"></i>
             </span>
-          </div>
+      </div>
       `);
     });
-  
+
+    console.log("creating task: " + textInput.value);
+    console.log("creating date: " + dateInput.value);
+    console.log("creating description: " + textarea.value);
+    console.log("creating progress: " + progressInput.value);
+    console.log(JSON.stringify(data));
+    
     resetForm();
   };
 
@@ -92,7 +116,7 @@ let createTasks = () => {
   let resetForm = () => {
     textInput.value = "";
     dateInput.value = "";
-    progressInput.value = "";
+    // progressInput.value = "";
     textarea.value = "";
   };
 
@@ -117,28 +141,46 @@ let createTasks = () => {
   
   /**
    * Edit a record
-   * @param {*} e 
+   * @param {*} e
    */
 
   let editTask = (e) => {
+
+    console.log(JSON.stringify(data));
+    console.log("editing task: " + textInput.value);
+    console.log("editing date: " + dateInput.value);    
+    console.log("editing description: " + textarea.value);
+    console.log("editing progress: " + progressInput.value);
+
     let selectedTask = e.parentElement.parentElement;
     
-    console.log(e);
+    console.log(selectedTask);
 
     textInput.value = selectedTask.children[0].innerHTML;
+    console.log("editing task " + textInput.value);
+    
     dateInput.value = selectedTask.children[1].innerHTML;
-    progressInput.value = selectedTask.children[2].innerHTML;
-    console.log("editing progress " + progressInput.value);
-    textarea.value = selectedTask.children[3].innerHTML;
+    console.log("editing date " + dateInput.value);
+    
+    
+    textarea.value = selectedTask.children[2].innerHTML;
     console.log("editing description " + textarea.value);
   
+    progressInput.value = selectedTask.children[3].innerHTML;
+    console.log("editing progress " + progressInput.value);
+    
     deleteTask(e);
+    console.log(JSON.stringify(data));
   };
   
 
+  /**
+   * Get data from local storage
+   */
+
   (() => {
     data = JSON.parse(localStorage.getItem("data")) || [];
-    console.log(data);
+    console.log("Get data from local storage" + data);
     createTasks();
   })();
   
