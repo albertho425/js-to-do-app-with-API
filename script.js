c463ad29bcb16dc52ed34dc386191a93be59aa84
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-06-04 20:15:03 -0700

    Move API Key to seperate file

--- script.js
+++ script.js
@@ -4,10 +4,6 @@
  */
 
 
-weatherAPIKey = "ca80ffda470e4eca8e4235808230905";
-mapAPIKey = "aG8NIzp3QrvPAfPAcatmWUYjhlHsaOcy";
-geoAPIkey = "eec122638c9e4b6397e0c2634019c4bc"
-
 //variables for form and modal
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
